0e702a7a6f6ae3164f11e9cd781d36f1de9483c4
overdd <lmbib3a4@duck.com>
2023-07-16 22:57:01 +0200

    graphql initial commit

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ app.get('/', (req, res) => {
     res.send('GraphQL is amazing');
 });
 
-const root = { product: ()=> { return { id: 23131312, name: "Widget", description: "Fake description", price: 34.99, soldout: false}}};
+const root = { product: ()=> {
+     return {
+        id: 23131312,
+        name: "Widget",
+        description: "Fake description",
+        price: 34.99,
+        soldout: false,
+        stores: [
+            { store: "Pasadena"},
+            { store: "LA"}
+        ],    
+    }}};
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
--- schema.js
+++ schema.js
@@ -1,16 +1,21 @@
 import { buildSchema } from "graphql";
 
 export const schema = buildSchema(`
-    type Query {
-        product: Product
-    }
-
-    type Product {
+        type Product {
         id: ID
         name: String
         description: String
         price: Float
         soldout: Boolean
+        stores: [Store]!
+    }
+    
+    type Query {
+        product: Product
+    }
+
+    type Store {
+        store: String
     }
 `)
 
